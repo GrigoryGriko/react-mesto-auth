2112ab999ad5ecff49b0b6d317bb14c23a366cf6
Grigory Griko <griko1996@gmail.com>
2022-09-22 16:55:44 +0300

    feat: реализация аутентификации

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,32 +1,67 @@
 import React from 'react';
 
-function Register() {
-  return (
-    <main className="sign-form">
-      <form className="sign-form__form">
-        <h2 className="sign-form__header">Регистрация</h2>
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <input
-          className="sign-form__input-email general-input-sign-form"
-          placeholder="Email"
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    //обработка логики регистрации
+  }
+
+  render() {
+    return (
+      <main className="sign-form">
+        <form
+          className="sign-form__form"
+          onSubmit={this.handleSubmit}
         >
+          <h2 className="sign-form__header">Регистрация</h2>
 
-        </input>
+          <input
+            id="email"
+            className="sign-form__input-email general-input-sign-form"
+            placeholder="Email"
+            type="text"
+            value={this.state.email}
+            onChange={this.handleChange}
+          >
 
-        <input
-          className="sign-form__input-password general-input-sign-form"
-          placeholder="Пароль"
-          type="password"
-        >
+          </input>
 
-        </input>
+          <input
+            id="password"
+            className="sign-form__input-password general-input-sign-form"
+            placeholder="Пароль"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          >
 
-        <button className="sign-form__button-submit">Зарегистрироваться</button>
+          </input>
 
-        <a href="/sign-in" className="sign-form__link">Уже зарегистрированы? Войти</a>
-      </form>
-    </main>
-  )
+          <button className="sign-form__button-submit">Зарегистрироваться</button>
+
+          <a href="/sign-in" className="sign-form__link">Уже зарегистрированы? Войти</a>
+        </form>
+      </main>
+    )
+  }
 }
 
 export default Register;
