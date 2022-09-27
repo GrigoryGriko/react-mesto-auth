da5d7085d2315bd31123866acd9fc68c134f470a
Grigory Griko <griko1996@gmail.com>
2022-09-27 11:54:48 +0300

    feat: настроен выход из аккаунта

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 function Header(props) {
+  const history = useHistory();
+
+  function signOut() {
+    localStorage.removeItem('jwt');
+    history.push('/login');
+  }
+
   return (
     <header className="header section">
       <a className="logo" href="#"></a>
 
       <div className="header__wrapper-auth">
         <p className="header__email">{props.userData.email}</p>
-        <p to="/sign-in" className="header__logout">Выйти</p>
+        <p onClick={signOut} to="/sign-in" className="header__logout">Выйти</p>
       </div>
     </header>
   )
