1a87626536bf80d6174e032e7cde6d1772001294
Grigory Griko <griko1996@gmail.com>
2022-09-27 16:15:27 +0300

    feat: настройка вариантов header с помощью роутов

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,38 +11,27 @@ function Header(props) {
   }
 
   return (
-    <Switch>
-      <Route path='/'>
+    
         <header className="header section">
           <a className="logo" href="#"></a>
 
           <div className="header__wrapper-auth">
-            <p className="header__email">{props.userData.email}</p>
-            <button onClick={signOut} to="/sign-in" className="header__action-auth">Выйти</button>
+          <Switch>
+            <Route path='/'>
+              <p className="header__email">{props.userData.email}</p>
+              <button onClick={signOut} to="/sign-in" className="header__action-auth">Выйти</button>
+            </Route>
+
+            <Route>
+              <Link to="/sign-in" className="header__action-auth">Вход</Link>
+            </Route>
+
+            <Route>
+              <Link to="/sign-up" className="header__action-auth">Регистрация</Link>
+            </Route>
+          </Switch>
           </div>
         </header>
-      </Route>
-
-      <Route path='/sign-up'>
-        <header className="header section">
-          <a className="logo" href="#"></a>
-
-          <div className="header__wrapper-auth">
-            <Link to="/sign-ip" className="header__action-auth">Вход</Link>
-          </div>
-        </header>
-      </Route>
-
-      <Route path='/sign-in'>
-        <header className="header section">
-          <a className="logo" href="#"></a>
-
-          <div className="header__wrapper-auth">
-            <Link to="/sign-up" className="header__action-auth">Регистрация</Link>
-          </div>
-        </header>
-      </Route>
-    </Switch>
   )
 }
 
