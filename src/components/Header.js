f9cb0e0671fac235b1e19eac05d1e5e496f69e85
Grigory Griko <griko1996@gmail.com>
2022-09-27 10:40:12 +0300

    style: вёрстка header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Header(props) {
   return (
     <header className="header section">
       <a className="logo" href="#"></a>
-      <p>{props.userData.email}</p>
+      <p className="header__email">{props.userData.email}</p>
+      <Link to="/sign-in" className="header__logout">Выйти</Link>
     </header>
   )
 }
